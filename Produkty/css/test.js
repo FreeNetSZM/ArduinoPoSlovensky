f365a91c46d1a6c4115ee095d2209c06521923a4
FreeNetSZM <freenet.szm@seznam.cz>
2017-03-04 17:35:36 +0100

    Update test.js

--- Produkty/css/test.js
+++ Produkty/css/test.js
@@ -1,27 +1,32 @@
-function ProduktID(id) {
-var div = document.createElement("div");
-div.innerHTML =
-    '<div id="banner_div">' +
-    '<div id="DIV_1">\n' +
-    '<div id="DIV_2">\n' +
-    '<img src="/ArduinoPoSlovensky/Produkty/images/UNO-R3-developent-board-MEGA328P-With-USB-Cable.jpg" id="IMG_3"/>\n' +
-    '</div>\n' +
-    '<div id="DIV_4">\n' +
-    '<h5 id="H5_5">Arduino UNO R3 s USB káblom </h5>Arduino UNO R3 s čipom Atmega 328p v DIP púzdre dodávaný s USB káblom \n' +
-    '</div>\n' +
-    '<div id="DIV_6">\n' +
-    '</div>\n' +
-    '<div id="DIV_15">\n' +
-    '<div id="DIV_16">\n' +
-    '<span id="SPAN_17">cena</span>\n' +
-    '<a href="http://s.click.aliexpress.com/e/uF6UrZR" target="_blank" id="A_18">\n' +
-    '<i id="I_19"></i> Kúp\n' +
-    '<i id="I_20"></i></a>\n' +
-    '</div>\n' +
-    '<a href="http://s.click.aliexpress.com/e/vR7maIuzV" id="A_21">\n' +
-    '<img src="/ArduinoPoSlovensky/Produkty/images/ali.png" target="_blank" id="IMG_22"/></a>\n' +
-    '</div>\n' +
-    '</div>\n' +
-    '</div>\n';
-     document.getElementById(id).appendChild(div);
-} 
+  function ProduktID(id, nadpis, popis) {
+  var div = document.createElement(div);
+  div.innerHTML =
+      '<div id="banner_div">\n' +
+      '<div id="DIV_1">\n' +
+      '<div id="DIV_2">\n' +
+      '<img class="IMG_3" id="IMG_' + id +'"/>\n' +
+      '</div>\n' +
+      '<div id="DIV_4">\n' +
+      '<h5 id="H5_5">' + nadpis + '</h5>' + popis + ' \n' +
+      '</div>\n' +
+      '<div id="DIV_6">\n' +
+      '</div>\n' +
+      '<div id="DIV_15">\n' +
+      '<div id="DIV_16">\n' +
+      '<span id="SPAN_17"><div id="' + id + '"></div></span>\n' +
+      '<a href="http://s.click.aliexpress.com/e/uF6UrZR" target="_blank" id="A_18">\n' +
+      '<i id="I_19"></i> Kúp\n' +
+      '<i id="I_20"></i></a>\n' +
+      '</div>\n' +
+      '<a href="http://s.click.aliexpress.com/e/vR7maIuzV" id="A_21">\n' +
+      '<img src="/ArduinoPoSlovensky/Produkty/images/ali.png" target="_blank" id="IMG_22"/></a>\n' +
+      '</div>\n' +
+      '</div>\n' +
+      '</div>\n';
+     document.getElementById("inner").appendChild(div);
+      $.getJSON('https://gw.api.alibaba.com/openapi/param2/2/portals.open/api.getPromotionProductDetail/38404?fields=salePrice,discount,imageUrl&localCurrency=EUR&productId=' +id, function(data) {
+         if (data.result.discount == "0%") {document.getElementById(id).innerHTML = data.result.salePrice + "&nbsp&nbsp";}
+           else {document.getElementById(id).innerHTML = "aktuálna zľava: " + data.result.discount + "&nbsp&nbsp&nbsp" + data.result.salePrice + "&nbsp&nbsp";}
+        document.getElementById("IMG_" + id).src = data.result.imageUrl;
+         });  
+  } 
