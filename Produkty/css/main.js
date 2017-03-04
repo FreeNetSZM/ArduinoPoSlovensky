4e371d767c3e603e3f46023cf6ca6411e6d4d783
FreeNetSZM <freenet.szm@seznam.cz>
2017-03-04 08:47:59 +0100

    Update main.js

--- Produkty/css/main.js
+++ Produkty/css/main.js
@@ -1,5 +1,5 @@
 function cenaID(id) {
-      $.getJSON('https://gw.api.alibaba.com/openapi/param2/2/portals.open/api.getPromotionProductDetail/38404?fields=originalPrice,salePrice,discount&localCurrency=EUR&productId=' + ID, function(data) {
+      $.getJSON('https://gw.api.alibaba.com/openapi/param2/2/portals.open/api.getPromotionProductDetail/38404?fields=originalPrice,salePrice,discount&localCurrency=EUR&productId=' + id, function(data) {
       var zlava = data.result.discount;
       if (zlava == "0%") {var cena = data.result.salePrice + "&nbsp&nbsp";}
         else {var cena = "aktuálna zľava: " + zlava + "&nbsp&nbsp&nbsp" + data.result.salePrice + "&nbsp&nbsp";}
