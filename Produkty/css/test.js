a61f4f19519c6bd5e942f47508d6d09ba902b1a1
FreeNetSZM <freenet.szm@seznam.cz>
2017-03-08 19:01:58 +0100

    Create test.js

---
+++ Produkty/css/test.js
@@ -0,0 +1,6 @@
+        myObj = {
+            "name":"John",
+            "age":30,
+            "130":[ "Ford", "BMW", "Fiat" ]
+        };
+        document.getElementById(id).innerHTML = myObj[id][popis] + "&nbsp&nbsp" + myObj[id][nadpis];
