b54da97c182983405d8538f20e10119e24529bab
FreeNetSZM <freenet.szm@seznam.cz>
2017-03-08 19:39:30 +0100

    Update test.js

--- Produkty/css/test.js
+++ Produkty/css/test.js
@@ -1,6 +1,44 @@
-        myObj = {
-            "name":"John",
-            "age":30,
-            "130":[ "Ford", "BMW", "Fiat" ]
-        };
-        document.getElementById(id).innerHTML = myObj[id][popis] + "&nbsp&nbsp" + myObj[id][nadpis];
+function ProduktID(id) { 
+        
+  var div = document.createElement(div);
+  div.innerHTML =
+      '<div id="banner_div">\n' +
+        '<div id="banner_shadow">\n' +
+          '<div id="PRODUKT_IMG">\n' +
+            '<img class="ID_IMG" id="IMG_' + id +'"/>\n' +
+          '</div>\n' +
+          '<div id="POPIS">\n' +
+            '<h5 id="NADPIS"></h5>\n' +
+             '<div id="OPIS"></div>\n' +
+          '</div>\n' +
+          '<div id="DOWN">\n' +
+            '<div id="DOWN_R">\n' +
+              '<span id="PRICE"><div id="' + id + '"></div></span>\n' +
+              '<a target="_blank" id="KUP">\n' +
+              '<i id="KUP_PRED"></i> Kúp\n' +
+              '<i id="KUP_ZA"></i></a>\n' +
+            '</div>\n' +
+            '<a href="http://s.click.aliexpress.com/e/vR7maIuzV">\n' +
+            '<img src="/ArduinoPoSlovensky/Produkty/images/ali.png" target="_blank" id="IMG_ALI"/></a>\n' +
+          '</div>\n' +
+        '</div>\n' +
+      '</div>\n';
+  document.getElementById("inner").appendChild(div);
+    $.getJSON('https://gw.api.alibaba.com/openapi/param2/2/portals.open/api.getPromotionProductDetail/38404?fields=salePrice,discount,imageUrl,productUrl&localCurrency=EUR&productId=' +id, function(data) {
+     if (data.result.discount == "0%") {document.getElementById(id).innerHTML = data.result.salePrice + "&nbsp&nbsp";}
+       else {document.getElementById(id).innerHTML = "aktuálna zľava: " + data.result.discount + "&nbsp&nbsp&nbsp" + data.result.salePrice + "&nbsp&nbsp";}
+  });  
+  
+  $.getJSON('https://gw.api.alibaba.com/openapi/param2/2/portals.open/api.getAppPromotionProduct/38404?productId=' +id, function(img) {
+     document.getElementById("IMG_" + id).src = img.result.image220;    
+  });        
+        
+  $.getJSON('https://freenetszm.github.io/ArduinoPoSlovensky/Produkty/css/main.json', function(data) {
+     document.getElementById("NADPIS").innerHTML = data[id][0];
+     document.getElementById("OPIS").innerHTML = data[id][1];
+     document.getElementById("KUP").href = data[id][2]; 
+  });  
+        
+}
+
+
