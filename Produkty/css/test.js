fa3c6c411429d3cdd7a5c1c5db06caff29fd874d
FreeNetSZM <freenet.szm@seznam.cz>
2017-03-10 09:03:26 +0100

    Update test.js

--- Produkty/css/test.js
+++ Produkty/css/test.js
@@ -41,7 +41,7 @@ function ProduktID() {
      }
   });  
   
-  /*
+  
   var z;
   for (z = 0; z < max; z++) {
     
@@ -54,7 +54,7 @@ function ProduktID() {
          document.getElementById("IMG_" + z).src = img.result.image220;    
       });   
     })(z);
-  }*/
+  }
 
  
 }
