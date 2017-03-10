02ac7e03f58c28e85bbfbd8bd49fec7d190c6b8c
FreeNetSZM <freenet.szm@seznam.cz>
2017-03-10 09:04:24 +0100

    Update test.js

--- Produkty/css/test.js
+++ Produkty/css/test.js
@@ -46,6 +46,7 @@ function ProduktID() {
   for (z = 0; z < max; z++) {
     
     (function(z) {
+      id = arg[i];
       $.getJSON('https://gw.api.alibaba.com/openapi/param2/2/portals.open/api.getPromotionProductDetail/38404?fields=salePrice,discount,imageUrl,productUrl&localCurrency=EUR&productId=' +id, function(data) {
            if (data.result.discount == "0%") {document.getElementById(z).innerHTML = data.result.salePrice + "&nbsp&nbsp";}
              else {document.getElementById(z).innerHTML = "aktuálna zľava: " + data.result.discount + "&nbsp&nbsp&nbsp" + data.result.salePrice + "&nbsp&nbsp";}
