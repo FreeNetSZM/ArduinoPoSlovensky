66a58fc6be757c8bac4aa36131542ab4350a4549
FreeNetSZM <freenet.szm@seznam.cz>
2017-03-13 13:03:00 +0100

    Update test.js

--- Produkty/css/test.js
+++ Produkty/css/test.js
@@ -24,7 +24,7 @@ function ProduktID() {
                 '</div>\n' +
                 '<a href="http://s.click.aliexpress.com/e/vR7maIuzV">\n' +
                   '<img src="/ArduinoPoSlovensky/Produkty/images/ali.png" target="_blank" id="IMG_ALI"/></a>\n' +
-                '<div class="star-ratings-sprite"><span style="width:69%" class="star-ratings-sprite-rating"><div id="STAR_' + id + '"></div></span></div>\n' +
+                '<div class="star-ratings-sprite"><span style="width:69%" id="STAR_' + id + '" class="star-ratings-sprite-rating"><div id="SCORE_' + id + '"></div></span></div>\n' +
               '</div>\n' +
             '</div>\n' +
           '</div>\n'
@@ -47,7 +47,7 @@ function ProduktID() {
            if (data.result.discount == "0%") {document.getElementById(i).innerHTML = data.result.salePrice + "&nbsp&nbsp";}
              else {document.getElementById(i).innerHTML = "aktuálna zľava: " + data.result.discount + "&nbsp&nbsp&nbsp" + data.result.salePrice + "&nbsp&nbsp";}
         document.getElementById("STAR_" + i).style.width = (data.result.evaluateScore * 20) + "%";
-        document.getElementById("STAR_" + i).innerHTML = data.result.evaluateScore;
+        document.getElementById("SCORE_" + i).innerHTML = data.result.evaluateScore;
       });      
       $.getJSON('https://gw.api.alibaba.com/openapi/param2/2/portals.open/api.getAppPromotionProduct/38404?productId=' +id, function(img) {
          document.getElementById("IMG_" + i).src = img.result.image220;    
