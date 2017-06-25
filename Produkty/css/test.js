a8a4b3dcc0e1c4f4155fbeb6c17c890f14048d94
FreeNetSZM <freenet.szm@seznam.cz>
2017-06-25 13:33:39 +0200

    Update test.js

--- Produkty/css/test.js
+++ Produkty/css/test.js
@@ -2,8 +2,8 @@ function ProduktID() {
   var arg = arguments;
   $.getJSON('https://freenetszm.github.io/ArduinoPoSlovensky/Produkty/css/main.json', function(name) { 
     for (var id in name) {          
-      if (name[id][3].indexOf(arg[0]) > -1) {      
-        console.log(name[id][3].indexOf(arg[0]));
+      console.log(name[id][3].indexOf(arg[0]));
+      if (name[id][3].indexOf(arg[0]) > -1) {           
         div = document.createElement("div");
         div.setAttribute("id", id);
         div.setAttribute("class", "bannerID");
