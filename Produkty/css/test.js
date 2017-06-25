778857d3cc6a1761f7b844d484ad4697c72c2930
FreeNetSZM <freenet.szm@seznam.cz>
2017-06-25 11:52:07 +0200

    Update test.js

--- Produkty/css/test.js
+++ Produkty/css/test.js
@@ -4,6 +4,7 @@ function ProduktID() {
     for (var id in name) {       
       console.log(id);
       console.log(name[id][3].indexOf(arg));
+      console.log(name[id][0].indexOf("modul"));
       if (name[id][3].indexOf(arg) > -1) {      
         console.log(name[id][0]);
         }
